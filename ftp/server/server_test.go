de1cf6a0e66c3eac72908e6a77e4ef32d49b0a88
qsliu <qsliu2017@outlook.com>
2021-11-01 12:07:30 +0800

    make tests clean

--- ftp/server/server_test.go
+++ ftp/server/server_test.go
@@ -18,6 +18,7 @@ func readReply(c net.Conn) {
 }
 
 func assertReply(t *testing.T, c net.Conn, expect, msg string) {
+	t.Helper()
 	if readReply(c); strings.Compare(expect, string(__buffer[:__n])) != 0 {
 		t.Error(msg,
 			"\nExpect:", []byte(expect),
@@ -28,6 +29,7 @@ func assertReply(t *testing.T, c net.Conn, expect, msg string) {
 
 // Setup a mock connection, test if service ready, and return the client conn.
 func setupConn(t *testing.T) net.Conn {
+	t.Helper()
 	c, s := net.Pipe()
 	go handleConn(s)
 
@@ -36,7 +38,8 @@ func setupConn(t *testing.T) net.Conn {
 	return c
 }
 
-func endupConn(t *testing.T, c net.Conn) {
+func teardownConn(t *testing.T, c net.Conn) {
+	t.Helper()
 	c.Write([]byte(cmd.QUIT))
 	assertReply(t, c, "221 Service closing control connection.\r\n", "Service quit error")
 
@@ -53,100 +56,106 @@ func Test_Quit(t *testing.T) {
 
 func Test_Noop(t *testing.T) {
 	c := setupConn(t)
-	defer endupConn(t, c)
+	defer teardownConn(t, c)
 
 	c.Write([]byte(cmd.NOOP))
 	assertReply(t, c, "200 Command okay.\r\n", "Noop error")
 }
 
-func Test_User_with_valid_username(t *testing.T) {
-	c := setupConn(t)
-	defer endupConn(t, c)
+func Test_User(t *testing.T) {
+	t.Run("valid username", func(t *testing.T) {
+		c := setupConn(t)
+		defer teardownConn(t, c)
 
-	c.Write([]byte(fmt.Sprintf(cmd.USER, "test")))
-	assertReply(t, c, "331 User name okay, need password.\r\n", "test valid user name error")
+		c.Write([]byte(fmt.Sprintf(cmd.USER, "test")))
+		assertReply(t, c, "331 User name okay, need password.\r\n", "test valid user name error")
+	})
+
+	t.Run("invalid username", func(t *testing.T) {
+		c := setupConn(t)
+		defer teardownConn(t, c)
+
+		c.Write([]byte(fmt.Sprintf(cmd.USER, "test1")))
+		assertReply(t, c, "332 Need account for login.\r\n", "test invalid user name error")
+	})
 }
 
-func Test_User_with_invalid_username(t *testing.T) {
-	c := setupConn(t)
-	defer endupConn(t, c)
+func Test_Pass(t *testing.T) {
+	t.Run("without username", func(t *testing.T) {
+		c := setupConn(t)
+		defer teardownConn(t, c)
 
-	c.Write([]byte(fmt.Sprintf(cmd.USER, "test1")))
-	assertReply(t, c, "332 Need account for login.\r\n", "test invalid user name error")
-}
+		c.Write([]byte(fmt.Sprintf(cmd.PASS, "test")))
+		assertReply(t, c, "503 Bad sequence of commands.\r\n", "test pass bad sequence error")
+	})
 
-func Test_Pass_without_username(t *testing.T) {
-	c := setupConn(t)
-	defer endupConn(t, c)
+	t.Run("valid account", func(t *testing.T) {
+		c := setupConn(t)
+		defer teardownConn(t, c)
 
-	c.Write([]byte(fmt.Sprintf(cmd.PASS, "test")))
-	assertReply(t, c, "503 Bad sequence of commands.\r\n", "test pass bad sequence error")
-}
+		c.Write([]byte(fmt.Sprintf(cmd.USER, "test")))
+		assertReply(t, c, "331 User name okay, need password.\r\n", "test valid user name error")
 
-func Test_Pass_with_valid_account(t *testing.T) {
-	c := setupConn(t)
-	defer endupConn(t, c)
+		c.Write([]byte(fmt.Sprintf(cmd.PASS, "test")))
+		assertReply(t, c, "230 User logged in, proceed.\r\n", "test valid account error")
+	})
 
-	c.Write([]byte(fmt.Sprintf(cmd.USER, "test")))
-	assertReply(t, c, "331 User name okay, need password.\r\n", "test valid user name error")
+	t.Run("invalid account", func(t *testing.T) {
+		c := setupConn(t)
+		defer teardownConn(t, c)
 
-	c.Write([]byte(fmt.Sprintf(cmd.PASS, "test")))
-	assertReply(t, c, "230 User logged in, proceed.\r\n", "test valid account error")
-}
+		c.Write([]byte(fmt.Sprintf(cmd.USER, "pikachu")))
+		assertReply(t, c, "331 User name okay, need password.\r\n", "test valid user name error")
 
-func Test_Pass_with_invalid_account(t *testing.T) {
-	c := setupConn(t)
-	defer endupConn(t, c)
+		c.Write([]byte(fmt.Sprintf(cmd.PASS, "pikachu")))
+		assertReply(t, c, "530 Not logged in.\r\n", "test invalid account error")
 
-	c.Write([]byte(fmt.Sprintf(cmd.USER, "pikachu")))
-	assertReply(t, c, "331 User name okay, need password.\r\n", "test valid user name error")
-
-	c.Write([]byte(fmt.Sprintf(cmd.PASS, "pikachu")))
-	assertReply(t, c, "530 Not logged in.\r\n", "test invalid account error")
-
-	// After a failed login, username should be forgotten
-	c.Write([]byte(fmt.Sprintf(cmd.PASS, "winnie")))
-	assertReply(t, c, "503 Bad sequence of commands.\r\n", "test pass bad sequence error")
-}
-
-func Test_Port_without_login(t *testing.T) {
-	c := setupConn(t)
-	defer endupConn(t, c)
-
-	c.Write([]byte(fmt.Sprintf(cmd.PORT, 0, 0, 0, 0, 0, 0)))
-	assertReply(t, c, "530 Not logged in.\r\n", "test port with login error")
+		// After a failed login, username should be forgotten
+		c.Write([]byte(fmt.Sprintf(cmd.PASS, "winnie")))
+		assertReply(t, c, "503 Bad sequence of commands.\r\n", "test pass bad sequence error")
+	})
 }
 
 func Test_Port(t *testing.T) {
-	c := setupConn(t)
-	defer endupConn(t, c)
+	t.Run("not login", func(t *testing.T) {
+		c := setupConn(t)
+		defer teardownConn(t, c)
 
-	c.Write([]byte(fmt.Sprintf(cmd.USER, "test")))
-	assertReply(t, c, "331 User name okay, need password.\r\n", "test valid user name error")
-	c.Write([]byte(fmt.Sprintf(cmd.PASS, "test")))
-	assertReply(t, c, "230 User logged in, proceed.\r\n", "test valid account error")
+		c.Write([]byte(fmt.Sprintf(cmd.PORT, 0, 0, 0, 0, 0, 0)))
+		assertReply(t, c, "530 Not logged in.\r\n", "test port with login error")
+	})
 
-	// Client listens on a port
-	dataConn, err := net.Listen("tcp", ":5456")
-	if err != nil {
-		t.Fatal(err)
-	}
-	defer dataConn.Close()
+	t.Run("login", func(t *testing.T) {
+		c := setupConn(t)
+		defer teardownConn(t, c)
 
-	accept := make(chan struct{})
-	go func() {
-		conn, err := dataConn.Accept()
+		c.Write([]byte(fmt.Sprintf(cmd.USER, "test")))
+		assertReply(t, c, "331 User name okay, need password.\r\n", "test valid user name error")
+		c.Write([]byte(fmt.Sprintf(cmd.PASS, "test")))
+		assertReply(t, c, "230 User logged in, proceed.\r\n", "test valid account error")
+
+		// Client listens on a port
+		dataConn, err := net.Listen("tcp", ":5456")
 		if err != nil {
-			t.Log(err)
+			t.Fatal(err)
 		}
-		defer conn.Close()
-		accept <- struct{}{}
-	}()
+		defer dataConn.Close()
 
-	// Client sends the port to server
-	c.Write([]byte(fmt.Sprintf(cmd.PORT, 127, 0, 0, 1, 21, 80)))
-	assertReply(t, c, "200 Command okay.\r\n", "test port error")
-	if _, has := <-accept; !has {
-		t.Error("data port not accept any connection")
-	}
+		accept := make(chan struct{})
+		go func() {
+			conn, err := dataConn.Accept()
+			if err != nil {
+				t.Log(err)
+			}
+			defer conn.Close()
+			accept <- struct{}{}
+		}()
+
+		// Client sends the port to server
+		c.Write([]byte(fmt.Sprintf(cmd.PORT, 127, 0, 0, 1, 21, 80)))
+		assertReply(t, c, "200 Command okay.\r\n", "test port error")
+		if _, has := <-accept; !has {
+			t.Error("data port not accept any connection")
+		}
+	})
 }
