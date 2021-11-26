d616f6f1d0ea7e094afdb5de67ae23269da19517
qsliu <qsliu2017@outlook.com>
2021-11-26 20:10:34 +0800

    impl Block Mode, refactor file op.

--- ftp/client/file.go
+++ ftp/client/file.go
@@ -5,6 +5,7 @@ import (
 	"errors"
 	"ftp/cmd"
 	"ftp/fm"
+	"ftp/fm/block"
 	"io"
 	"io/fs"
 )
@@ -14,16 +15,6 @@ var (
 )
 
 func (client *clientImpl) Store(local, remote string) (err error) {
-	switch client.GetMode() {
-	case ModeStream:
-		err = client.storeStreamMode(local, remote)
-	default:
-		err = ErrModeNotSupported
-	}
-	return
-}
-
-func (client *clientImpl) storeStreamMode(local, remote string) error {
 	localFile := fm.OpenFile(local)
 	if localFile == nil {
 		return fs.ErrNotExist
@@ -38,6 +29,22 @@ func (client *clientImpl) storeStreamMode(local, remote string) error {
 		return errors.New(msg)
 	}
 
+	switch client.GetMode() {
+	case ModeStream:
+		err = client.storeStreamMode(localFile)
+	case ModeBlock:
+		err = client.storeBlockMode(localFile)
+	default:
+		err = ErrModeNotSupported
+	}
+	if _, msg, err := client.ctrlConn.Reader.ReadResponse(cmd.StatusFileActionCompleted); err != nil {
+		return errors.New(msg)
+	}
+
+	return
+}
+
+func (client *clientImpl) storeStreamMode(localFile io.Reader) error {
 	if _, err := io.Copy(bufio.NewWriter(client.dataConn), bufio.NewReader(localFile)); err != nil {
 		return err
 	}
@@ -48,14 +55,18 @@ func (client *clientImpl) storeStreamMode(local, remote string) error {
 		return err
 	}
 
-	if _, msg, err := client.ctrlConn.Reader.ReadResponse(cmd.StatusFileActionCompleted); err != nil {
-		return errors.New(msg)
+	return nil
+}
+
+func (client *clientImpl) storeBlockMode(localFile io.Reader) (err error) {
+	if err := block.Send(client.dataConn, localFile, 1<<10); err != nil {
+		return err
 	}
 
 	return nil
 }
 
-func (client *clientImpl) Retrieve(local, remote string) error {
+func (client *clientImpl) Retrieve(local, remote string) (err error) {
 	localFile := fm.CreateFile(local)
 	if localFile == nil {
 		return fs.ErrNotExist
@@ -65,18 +76,44 @@ func (client *clientImpl) Retrieve(local, remote string) error {
 	if err := client.createDataConn(); err != nil {
 		return err
 	}
-	defer client.closeDataConn() // In streaming mode, the data connection is closed after each file transfer.
 
 	if _, msg, err := client.cmd(cmd.ALREADY_OPEN, "RETR %s", remote); err != nil {
 		return errors.New(msg)
 	}
 
-	if _, err := io.Copy(localFile, client.dataConn); err != nil {
+	switch client.mode {
+	case ModeStream:
+		err = client.retrieveStreamMode(localFile)
+	case ModeBlock:
+		err = client.retrieveBlockMode(localFile)
+	default:
+		err = ErrModeNotSupported
+	}
+	if err != nil {
 		return err
 	}
 
 	if _, msg, err := client.ctrlConn.Reader.ReadResponse(cmd.StatusFileActionCompleted); err != nil {
 		return errors.New(msg)
+	}
+
+	return
+
+}
+
+func (client *clientImpl) retrieveStreamMode(localFile io.Writer) error {
+	defer client.closeDataConn() // In streaming mode, the data connection is closed after each file transfer.
+
+	if _, err := io.Copy(localFile, client.dataConn); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (client *clientImpl) retrieveBlockMode(localFile io.Writer) error {
+	if err := block.Receive(localFile, client.dataConn); err != nil {
+		return err
 	}
 
 	return nil
--- ftp/client/file_test.go
+++ ftp/client/file_test.go
@@ -4,6 +4,7 @@ import (
 	"bytes"
 	"crypto/md5"
 	"fmt"
+	"ftp/fm/block"
 	"io"
 	"net"
 	"net/textproto"
@@ -117,6 +118,132 @@ func TestStorStreamMode(t *testing.T) {
 	local, _ := os.OpenFile("test_files/small9993", os.O_RDONLY, 0666)
 	defer local.Close()
 	remote, _ := os.OpenFile("_test_/small9993", os.O_RDONLY, 0666)
+	defer remote.Close()
+
+	hasher := md5.New()
+	io.Copy(hasher, local)
+	localMd5 := hasher.Sum(nil)
+	hasher.Reset()
+	io.Copy(hasher, remote)
+	remoteMd5 := hasher.Sum(nil)
+	if !bytes.Equal(localMd5, remoteMd5) {
+		t.Fatalf("file not equal \n%x\n%x", localMd5, remoteMd5)
+	}
+}
+
+func TestStorBlockMode(t *testing.T) {
+	os.Mkdir("_test_", 0777)
+	defer os.RemoveAll("_test_")
+
+	filesave := make(chan bool)
+	go func() {
+		listener, _ := net.Listen("tcp", ":8972")
+		conn, _ := listener.Accept()
+		listener.Close()
+		server := textproto.NewConn(conn)
+		defer server.Close()
+
+		var dataConn net.Conn
+
+		server.Writer.PrintfLine("220 Service ready for new user.")
+		for {
+			if line, _ := server.ReadLine(); strings.HasPrefix(line, "PORT") {
+				var h1, h2, h3, h4, p1, p2 byte
+				fmt.Sscanf(line, "PORT %d,%d,%d,%d,%d,%d", &h1, &h2, &h3, &h4, &p1, &p2)
+				dataConn, _ = net.Dial("tcp", fmt.Sprintf("%d.%d.%d.%d:%d", h1, h2, h3, h4, int(p1)*256+int(p2)))
+				server.Writer.PrintfLine("200 Command okay.")
+			} else if strings.HasPrefix(line, "STOR ") {
+				if dataConn == nil {
+					server.Writer.PrintfLine("150 File status okay; about to open data connection.")
+					continue
+				}
+				server.Writer.PrintfLine("125 Data connection already open; transfer starting.")
+				f, _ := os.Create(path.Join("_test_", line[len("STOR "):]))
+				block.Receive(f, dataConn)
+				server.Writer.PrintfLine("250 Requested file action okay, completed.")
+				f.Close()
+				filesave <- true
+			} else if strings.HasPrefix(line, "MODE") {
+				server.Writer.PrintfLine("200 Command okay.")
+			}
+		}
+	}()
+
+	client, _ := NewFtpClient("localhost:8972")
+	client.Mode(ModeBlock)
+	if err := client.Store("test_files/small9993", "small9993"); err != nil {
+		t.Fatal(err)
+	}
+
+	<-filesave
+	// Don't go too fast
+
+	local, _ := os.OpenFile("test_files/small9993", os.O_RDONLY, 0666)
+	defer local.Close()
+	remote, _ := os.OpenFile("_test_/small9993", os.O_RDONLY, 0666)
+	defer remote.Close()
+
+	hasher := md5.New()
+	io.Copy(hasher, local)
+	localMd5 := hasher.Sum(nil)
+	hasher.Reset()
+	io.Copy(hasher, remote)
+	remoteMd5 := hasher.Sum(nil)
+	if !bytes.Equal(localMd5, remoteMd5) {
+		t.Fatalf("file not equal \n%x\n%x", localMd5, remoteMd5)
+	}
+}
+
+func TestRetrBlockMode(t *testing.T) {
+	os.Mkdir("_test_", 0777)
+	defer os.RemoveAll("_test_")
+
+	filesave := make(chan bool)
+	go func() {
+		listener, _ := net.Listen("tcp", ":8973")
+		conn, _ := listener.Accept()
+		listener.Close()
+		server := textproto.NewConn(conn)
+		defer server.Close()
+
+		var dataConn net.Conn
+
+		server.Writer.PrintfLine("220 Service ready for new user.")
+		for {
+			if line, _ := server.ReadLine(); strings.HasPrefix(line, "PORT") {
+				var h1, h2, h3, h4, p1, p2 byte
+				fmt.Sscanf(line, "PORT %d,%d,%d,%d,%d,%d", &h1, &h2, &h3, &h4, &p1, &p2)
+				dataConn, _ = net.Dial("tcp", fmt.Sprintf("%d.%d.%d.%d:%d", h1, h2, h3, h4, int(p1)*256+int(p2)))
+				server.Writer.PrintfLine("200 Command okay.")
+			} else if strings.HasPrefix(line, "RETR ") {
+				if dataConn == nil {
+					server.Writer.PrintfLine("150 File status okay; about to open data connection.")
+					continue
+				}
+				server.Writer.PrintfLine("125 Data connection already open; transfer starting.")
+				f, _ := os.Open(path.Join("test_files", line[len("RETR "):]))
+				block.Send(dataConn, f, 1<<8)
+				server.Writer.PrintfLine("250 Requested file action okay, completed.")
+				f.Close()
+				filesave <- true
+			} else if strings.HasPrefix(line, "MODE") {
+				server.Writer.PrintfLine("200 Command okay.")
+			}
+		}
+	}()
+
+	client, _ := NewFtpClient("localhost:8973")
+	client.Mode(ModeBlock)
+	if err := client.Retrieve("_test_/small9993", "small9993"); err != nil {
+		t.Fatal(err)
+	}
+
+	<-filesave
+	// Don't go too fast
+
+	local, _ := os.OpenFile("_test_/small9993", os.O_RDONLY, 0666)
+	defer local.Close()
+	remote, _ := os.OpenFile("test_files/small9993", os.O_RDONLY, 0666)
 	defer remote.Close()
 
 	hasher := md5.New()
